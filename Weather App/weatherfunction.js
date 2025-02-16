b3bc5f84b835e97f23a6c9abdc92d893b5e87586
Ramsey80 <104313940+Ramsey80@users.noreply.github.com>
2025-02-16 06:29:25 -0800

    Delete Weather App directory

--- Weather App/weatherfunction.js
+++
@@ -1,6 +0,0 @@
-// weatherFunction.js
-const makeWeatherReport = (city, temperature, condition) => {
-    return `The weather in ${city} is ${condition} with a temperature of ${temperature}°C.`;
-};
-
-export { makeWeatherReport };
